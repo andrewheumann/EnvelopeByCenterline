bb84861972d65d180337cbc38e9fd2f6d96e3af2
Andrew Heumann <andrew@hypar.io>
2020-02-12 11:47:01 -0500

    add sketch to function

--- src/EnvelopeByCenterline.cs
+++ src/EnvelopeByCenterline.cs
@@ -21,24 +21,26 @@ namespace EnvelopeByCenterline
             var perimeter = Centerline.Offset(input.BarWidth / 2, EndType.Butt).First();
 
             // Create the foundation Envelope.
-            var extrude = new Elements.Geometry.Solids.Extrude(perimeter, input.FoundationDepth, Vector3.ZAxis, 0.0, false);
+            var extrude = new Elements.Geometry.Solids.Extrude(perimeter, input.FoundationDepth, Vector3.ZAxis, false);
             var geomRep = new Representation(new List<Elements.Geometry.Solids.SolidOperation>() { extrude });
             var fndMatl = new Material("foundation", new Color(0.60000002384185791, 0.60000002384185791, 0.60000002384185791, 1), 0.0f, 0.0f);
             var envMatl = new Material("envelope", new Color(0.30000001192092896, 0.699999988079071, 0.699999988079071, 0.6), 0.0f, 0.0f);
             var envelopes = new List<Envelope>()
             {
                 new Envelope(perimeter, input.FoundationDepth * -1, input.FoundationDepth, Vector3.ZAxis,
-                             0.0, new Transform(0.0, 0.0, input.FoundationDepth * -1), fndMatl, geomRep, Guid.NewGuid(), "")
+                             0.0, new Transform(0.0, 0.0, input.FoundationDepth * -1), fndMatl, geomRep, false, Guid.NewGuid(), "")
             };
 
             // Create the Envelope at the location's zero plane.
             var output = new EnvelopeByCenterlineOutputs(input.BuildingHeight, input.FoundationDepth);
 
-            extrude = new Elements.Geometry.Solids.Extrude(perimeter, input.BuildingHeight, Vector3.ZAxis, 0.0, false);
+            extrude = new Elements.Geometry.Solids.Extrude(perimeter, input.BuildingHeight, Vector3.ZAxis, false);
             geomRep = new Representation(new List<Elements.Geometry.Solids.SolidOperation>() { extrude });
             envelopes.Add(new Envelope(perimeter, 0.0, input.BuildingHeight, Vector3.ZAxis, 0.0,
-                          new Transform(), envMatl, geomRep, Guid.NewGuid(), ""));
+                          new Transform(), envMatl, geomRep, false, Guid.NewGuid(), ""));
             output.model.AddElements(envelopes);
+            var sketch = new Sketch(input.Centerline, Guid.NewGuid(), "Centerline Sketch");
+            output.model.AddElement(sketch);
             return output;
         }
 
